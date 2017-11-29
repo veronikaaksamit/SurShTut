5af48901061c5edba4c1ac6cb85e68f98d53ba7d
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-29 17:33:42 +0100

    added possible gems to the project, PickupManager

---
+++ Assets/Scripts/Managers/PickupsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupsManager : MonoBehaviour {
+
+    public PlayerHealth playerHealth;
+    public GameObject pickup;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(pickup, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
