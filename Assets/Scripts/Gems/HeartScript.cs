f6e717992808d14bd9341cfa543a64573de4195a
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-30 20:14:15 +0100

    added scripts for speed and heart gems

---
+++ Assets/Scripts/Gems/HeartScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class HeartScript : MonoBehaviour {
+    
+
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth.currentHealth <= 90)
+            {
+                playerHealth.currentHealth += 30;
+            }
+        } 
+
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Assets/Scripts/Gems/SpeedScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedScript : MonoBehaviour {
+    
+
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+            playerMovement.speed = playerMovement.speed + 10;
+        } 
+
+        Destroy(gameObject);
+
+        //new WaitForSeconds(20);
+    }
+
+    /*IEnumerator<> waiter()
+    {
+    }*/
+
+}
