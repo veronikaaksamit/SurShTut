51b96bc48788b09f86228d07646f374cd43a5819
veronikaaksamit <veronika.aksamitova@gmail.com>
2017-11-30 17:19:11 +0100

    added scenes as levels to build settings, added script for loading different levels to all scenes

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Managers
+{
+    public class LevelManager:MonoBehaviour
+    {
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+                SceneManager.LoadScene("Level 01", LoadSceneMode.Single);
+
+            if (Input.GetKeyDown(KeyCode.F2))
+                SceneManager.LoadScene("Level 02", LoadSceneMode.Single);
+
+            if (Input.GetKeyDown(KeyCode.F3))
+                SceneManager.LoadScene("Level 03", LoadSceneMode.Single);
+
+            if (Input.GetKeyDown(KeyCode.F4))
+                SceneManager.LoadScene("Level 04", LoadSceneMode.Single);
+        }
+        
+    }
+}
